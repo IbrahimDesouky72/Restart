abf663d6a25174e4221ab04a44e40e21e3f26811
Ibrahim Desouky Ibrahim <ibrahim.desouky44@gmail.com>
2024-02-20 21:36:19 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ibrahim.desouky on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
